77084f5415b1441b391fd979e7ba473167727231
r-a-y <r-a-y@gmx.com>
2019-02-01 14:54:19 -0800

    Fix link padding in condensed fixed header.

--- functions.php
+++ functions.php
@@ -24,7 +24,7 @@ function gcdi_enqueue_scripts() {
 	}
 
 	// Update this whenever we make an update to bust cache.
-	$css_version = '20190131c';
+	$css_version = '20190201';
 	$js_version  = '20190201';
 
 	/* CSS ***************************************************/
