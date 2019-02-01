b7b8cb23ee9c96e72d41af764a94900fc238d702
r-a-y <r-a-y@gmx.com>
2019-02-01 14:49:43 -0800

    Add javascript.

---
+++ js/app.js
@@ -0,0 +1,8 @@
+jQuery(function($){
+	$('#site-header').sticky({
+		topSpacing: $('#wpadminbar').length ? 32 : 0, // Space between element and top of the viewport
+		zIndex: 100, // z-index
+		//stopper: "#bar" // Id, class, or number value
+		stickyClass: 'fixed-header' // Class applied to element when it's stuck
+	});
+});
