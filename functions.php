a50d28f40f349bcdcfd90bb7ea928b997289baba
r-a-y <r-a-y@gmx.com>
2019-02-06 11:51:56 -0800

    Position fix for condensed header when admin bar is showing.

--- functions.php
+++ functions.php
@@ -24,7 +24,7 @@ function gcdi_enqueue_scripts() {
 	}
 
 	// Update this whenever we make an update to bust cache.
-	$css_version = '20190205';
+	$css_version = '20190206';
 	$js_version  = '20190201';
 
 	/* CSS ***************************************************/
