eb53e81e4d1872977c2e67f8c65d306ed5a2b7e9
r-a-y <r-a-y@gmx.com>
2019-04-23 06:23:11 -0700

    Bump CSS version to bust cache.

--- functions.php
+++ functions.php
@@ -24,7 +24,7 @@ function gcdi_enqueue_scripts() {
 	}
 
 	// Update this whenever we make an update to bust cache.
-	$css_version = '20190408';
+	$css_version = '20190423';
 	$js_version  = '20190408';
 
 	/* CSS ***************************************************/
