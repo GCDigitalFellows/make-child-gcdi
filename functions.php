6bbe5b5172db23dd0f1ead8dbdbfb9e41fbfc055
r-a-y <r-a-y@gmx.com>
2020-04-09 15:18:20 -0700

    Revert "Ensure category colors use proper padding after latest Events Calendar update."
    Not necessary. Needed to uncheck the "Legend Superpowers" option under
    "Events > Settings > Category Colors" in the admin area.
    
    This reverts commit 1add0e9625827095ab2dc925252c4427585891da.

--- functions.php
+++ functions.php
@@ -24,7 +24,7 @@ function gcdi_enqueue_scripts() {
 	}
 
 	// Update this whenever we make an update to bust cache.
-	$css_version = '20200409';
+	$css_version = '20190423';
 	$js_version  = '20190408';
 
 	/* CSS ***************************************************/
